a8a1db72daf4314ec9bc70baabd45a79b5f364a7
796228 <gr8kaifzaidi78600@gmail.com>
2022-11-24 19:49:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState } from 'react';
 
 function App() {
+  const [city,setCity]=useState();
+  const [data,setData]=useState();
+  const handleChange=(e)=>{
+    console.log(e.target.value)
+    setCity(e.target.value)
+  }
+
+  const handleSubmit=()=>{
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=934bb5824ca864c69f012a8c87f2f09e`)
+    .then((res)=>{
+      console.log(res.data)
+      setData(res.data)
+      document.getElementById("input").value=""
+    })
+    .catch((err)=>{console.log(err)})
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="container">
+        <input type="text" id="input" placeholder="enter the city" onChange={handleChange} /><br></br>
+        <button id="button" onClick={handleSubmit}>Search</button>
+      </div>
+      <div id="weatherBox">
+        <h1>City:{data.name}</h1>
+        <h2>Tempearature:{(data.main.temp-273)}</h2>
+
+      </div>
     </div>
   );
 }
